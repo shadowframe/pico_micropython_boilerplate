00a5c37ddfca944f795fa10d5c8f9b651d978b8f
Jan <jan.weber123@googlemail.com>
2022-04-29 12:18:23 +0200

    test

---
+++ led.py
@@ -0,0 +1,15 @@
+from machine import Pin
+import utime
+
+led = Pin(17, Pin.OUT)
+button = Pin(16, Pin.IN, Pin.PULL_DOWN)
+
+while True:
+    if button.value() == 1:
+        led.value(1)
+        print("led on")
+        utime.sleep(2)
+        led.value(0)
+        print("led off")
+
+
